85766a1482536a3e2f862213458aa85d172453fe
mmascaro864 <marc.j.mascaro@gmail.com>
2023-03-05 11:28:04 -0500

    debugging

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -20,7 +20,7 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, DictVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.base import BaseEstimator, TransformerMixin
